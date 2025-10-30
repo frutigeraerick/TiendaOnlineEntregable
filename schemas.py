d83913a183359cd424fa0861fbc0f002a994f170
frutigeraerick <erei8377@gmail.com>
2025-10-30 00:02:04 -0500

    Se agrega base de validaciones para producto

--- schemas.py
+++ schemas.py
@@ -18,3 +18,10 @@ class CategoriaOut(CategoriaBase):
     id: int
     class Config:
         orm_mode = True
+
+class ProductoBase(BaseModel):
+    nombre: str = Field(..., min_length=2, max_length=100, description="Nombre del producto")
+    precio: float = Field(..., gt=0, description="Precio del producto (mayor que 0)")
+    stock: int = Field(..., ge=0, description="Cantidad en stock (no puede ser negativa)")
+    descripcion: Optional[str] = Field(None, max_length=300, description="Descripcion del producto")
+    activa: Optional[bool] = True
